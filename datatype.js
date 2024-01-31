c6d1188884200d148e1728a6486a3f6903077661
Rutik Kumbhar <128617896+Rutik126@users.noreply.github.com>
2024-01-31 11:27:55 +0000

    complete

---
+++ datatype.js
@@ -0,0 +1,17 @@
+//https://tc39.es
+
+let name = "rutik"
+let age = 18
+let work = null;
+
+//types of datatype
+// Number
+// BigInt
+// String
+// Boolean
+// null => standalone value
+// undefined
+
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
