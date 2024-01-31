c03c7c7344ab7869e36610474ee149796505bf8c
Rutik Kumbhar <128617896+Rutik126@users.noreply.github.com>
2024-01-31 12:43:13 +0000

    Done

---
+++ operations.js
@@ -0,0 +1,11 @@
+// https://tc39.es/ecma262/2020/#sec-abstract-operations
+
+let str1 = "Hello"
+let str2 = " Rutik"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1  + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
