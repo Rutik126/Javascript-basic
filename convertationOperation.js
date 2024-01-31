ab998f2944ef46f64d4e0923f127bf72d0034536
Rutik Kumbhar <128617896+Rutik126@users.noreply.github.com>
2024-01-31 11:52:18 +0000

    Done

---
+++ convertationOperation.js
@@ -0,0 +1,16 @@
+let score = "64ab"
+console.log(typeof score)
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)  
+
+// "64"=> 64
+// "64ab"=> NaN (Not a Number)
+
+
+let loggedIn = ""
+let BooleanloggedIn = Boolean(loggedIn)
+console.log(BooleanloggedIn);
+// true =>1
+// " "=>false 
---
+++ variable.js
@@ -0,0 +1,8 @@
+const accountId = 123454
+let email = "rutik@gmail.com"
+var Password = "12345"
+accountcity = "Sangli"
+let accountState;
+
+console.log(accountId);
+console.table([email, Password, accountcity,accountState])
