3787de999e2414ac7ba8d701a4f04c6262946717
Rutik Kumbhar <128617896+Rutik126@users.noreply.github.com>
2024-03-05 11:42:16 +0000

    date

---
+++ time.js
@@ -0,0 +1,9 @@
+let MyDate = new Date()
+console.log(MyDate.toDateString());
+
+let NewDate = new Date(29,1,23,45,3)
+console.log(NewDate.toDateString());
+
+let Timestamp = Date.now()
+console.log(Timestamp);
+console.log(NewDate.getTime());
